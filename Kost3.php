14ac50dbf1afbd2ab9edbc80f2559fb1919013ce
Rizky Gumelar Atmaja <reezkygatmaja21@student.uns.ac.id>
2017-06-20 22:52:13 +0700

    Add files via upload
    yang bagian Contact US tablenya belum bisa ngisi ke database.

---
+++ Kost3.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Finding Kost-mu!</title>
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<!-- Custom Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Mrs+Sheppards%7CDosis:300,400,700%7COpen+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800;' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top">
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+<div class="container">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand page-scroll" href="#page-top"><img src="img/find.png" style="height:60px;" alt="logo"></a>
+	</div>
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav navbar-right" type="text/css">
+			<li>
+			<a href="lihatkost.php">Home</a>
+			</li>
+			<li>
+			<a class="page-scroll" href="#gallery">Gallery</a>
+			</li>
+			<!--<li>
+			<a class="page-scroll" href="#contact">Contact</a>
+		</li>-->
+			<li>
+			<a class="page-scroll" href="#contact">Lihat Kost-an</a>
+			</li>
+			<li>
+			<a href="">Pemesan</a>
+			</li>
+			<li>
+			<a href="">Pendaftar</a>
+			</li>
+			<li>
+			<a href="http://localhost/finding/layana/lol/profile.php">Logout</a>
+			</li>
+		</ul>
+	</div>
+	<!-- /.navbar-collapse -->
+</div>
+<!-- /.container -->
+</nav>
+
+<!-- Section Intro Slider
+================================================== -->
+<div id="carousel-example-generic" class="carousel intro slide">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+	</ol>
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<!-- First slide -->
+		<div class="item active">
+				<img src="img/Kostan/Kost3/k.jpg" class="item active" style="width:100%;height:70px;" alt="kost">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+		<!-- /.item -->
+		<!-- Second slide -->
+		<div class="item">
+			<img src="img/Kostan/Kost3/k1.jpg" class="item" style="width:100%;height:70px;" alt="">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+		<!-- /.item -->
+		<!-- Third slide -->
+		<div class="item">
+			<img src="img/Kostan/Kost3/k2.jpg" class="item" style="width:100%;height:70px;" alt="">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+		<!-- 4-->
+		<div class="item">
+			<img src="img/Kostan/Kost3/k3.jpg" class="item" style="width:100%;height:70px;" alt="">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+    <!-- 5-->
+		<div class="item">
+			<img src="img/Kostan/Kost3/kk3.jpg" class="item" style="width:100%;height:70px;" alt="">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+    <!-- 6-->
+		<div class="item">
+			<img src="img/Kostan/Kost3/Fas2.jpg" class="item" style="width:100%;height:70px;" alt="">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+    <!-- 7-->
+		<div class="item">
+			<img src="img/Kostan/Kost3/Fas3.jpg" class="item" style="width:100%;height:70px;" alt="">
+			<div class="carousel-caption">
+				<h2 data-animation="animated bounceInDown">
+				Fasilitas </h2>
+				<h1 data-animation="animated bounceInUp">
+				"Kost Putri 3"</h1>
+			</div>
+		</div>
+		<!-- /.item -->
+	</div>
+	<!-- /.carousel-inner -->
+	<!-- Controls (currently displayed none from style.css)-->
+	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	<span class="sr-only">Next</span>
+	</a>
+</div>
+<!-- /.carousel -->
+<section id="about">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-10 col-lg-offset-1 text-center">
+			<h2 class="section-heading">DETAIL <b>KOST</b></h2>
+			<hr>
+			<div style="margin-left: -100px" align="justify">
+				<br/><br/>
+				<div style="font-size: 50px; color: #FF5733">
+					<?php
+						include 'koneksi.php';
+
+						//Query
+						$query = mysqli_query("select *  from detail_kost where kode = '1'", $koneksi);
+						$baris = mysql_fetch_array($query, MYSQL_ASSOC);
+						echo "".$baris["nama_kost"]."<br/><br/>";
+
+						mysql_close($koneksi);
+
+					 ?>
+				</div>
+
+				<div class="" style="margin-top: -30px;">
+					<?php
+					 include 'koneksi.php';
+
+					 //Query
+					 $query = mysql_query("select *  from detail_kost where kode = '1'", $koneksi);
+					 $baris = mysql_fetch_array($query, MYSQL_ASSOC);
+					 echo "Fasilitas Kamar" .str_repeat('&nbsp;', 15).": ".'&emsp;'.$baris["fk"]."<br/><br/>";
+					 echo "Luas Kamar" .str_repeat('&nbsp;', 21).": ".'&emsp;'.$baris["luas"]."<br/><br/>";
+					 echo "Kamar Mandi" .str_repeat('&nbsp;', 18).": ".'&emsp;'.$baris["km"]."<br/><br/>";
+					 echo "Fasilitas Umum" .str_repeat('&nbsp;', 15).": ".'&emsp;'.$baris["fu"]."<br/><br/>";
+					 echo "Parkir" .str_repeat('&nbsp;', 32).": ".'&emsp;'.$baris["parkir"]."<br/><br/>";
+					 echo "Akses Lingkungan" .str_repeat('&nbsp;', 11).": ".'&emsp;'.$baris["al"]."<br/><br/>";
+					 echo "Keterangan Lain" .str_repeat('&nbsp;', 14).": ".'&emsp;'.$baris["ket"]."<br/><br/>";
+					 echo "Contact Person" .str_repeat('&nbsp;', 16).": ".'&emsp;'.$baris["cp"]."<br/><br/>";
+					 echo "Alamat" .str_repeat('&nbsp;', 31).": ".'&emsp;'.$baris["alm"]."<br/><br/>";
+
+					 mysql_close($koneksi);
+
+					 ?>
+				 </div>
+			 </div>
+		 </div>
+</div>
+
+<div style=" background: #FFEE58; margin-left: 800px;">
+	<div class="" style="margin-left: 50px; color: black; padding-top: 20px">
+		<?php
+			include 'koneksi.php';
+
+			//Query
+			$query = mysql_query("select *  from detail_kost where kode = '3'", $koneksi);
+			$baris = mysql_fetch_array($query, MYSQL_ASSOC);
+			echo "Tersedia" .str_repeat('&nbsp;', 11).": ".'&emsp;'.$baris["jum"]."<br/><br/>";
+			echo "Harga" .str_repeat('&nbsp;', 15).": ".'&emsp; Rp &emsp;'.$baris["harga"]."<br/><br/>";
+
+			mysql_close($koneksi);
+
+		 ?>
+
+	</div>
+</div>
+</div>
+</section>
+
+<section id="contact">
+<div class="container">
+<div class="row">
+	<div class="col-md-8 col-md-offset-2 text-center">
+		<h2 class="section-heading">Tertarik ? Silahkan kirim pesan ke kami <b> Sekarang !</b></h2>
+		<hr class="primary">
+		<p>
+			 Bagi kalian calon penghuni kos yang tertarik dengan kamar yang tersedia, dapat menghubungi kami dibawah ini : 
+		</p>
+		<div class="regularform">
+			<div class="done">
+				<div class="alert alert-success">
+					<button type="button" class="close" data-dismiss="alert">×</button>
+					Pesan anda telah terkirim, Terimakasih telah berkunjung di Finding Kos-mu!	
+				</div>
+			</div>
+			<form method="post" action="contact.php" id="contactform" class="text-left">
+				<input name="nama" type="text" class="col-md-6 norightborder" placeholder="Your Name *">
+				<input name="no_telp" type="Nomor Telp/Hp Anda" class="col-md-6" placeholder="Nomor Telepon/Hp yang dapat dihubungi *">
+				<textarea name="message" class="col-md-12" placeholder="Message *"></textarea>
+				<input type="submit" id="submit" class="contact submit btn btn-primary btn-xl" value="Send message">
+			</form>
+		</div>
+	</div>
+</div>
+</div>
+</section>
+
+<section id="login">
+<div class="container regularform">
+<div class="row">
+	<div class="col-md-8 col-md-offset-2 text-center">
+		<h2 class="section-heading"> LOG <b>IN</b></h2>
+		<hr class="primary">
+			<div class="main">
+					<form action="" method="post">
+						<input style="margin-left: 190px" id="name" name="username" placeholder="username" type="text" class="col-md-6">
+						<div class="col-md-12">
+						</div>
+						<input style="margin-left: 190px" id="password" name="password" placeholder="**********" type="password" class="col-md-6">
+						<br/><br/><br/><br/><br/><br/><br/>
+						<input type="submit" name="submit" id="submit" value="Login">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+<!-- Section Footer
+================================================== -->
+<section class="bg-dark">
+<div class="container">
+<div class="row">
+	<div style="margin-top: 20px" class="col-md-12 text-center">
+		<h1 class="bottombrand wow flipInX">Finding Kost-mu!</h1>
+		<p>
+			&copy; 2016 Bootstrap HTML Template by WowThemes.net
+		</p>
+	</div>
+</div>
+</div>
+</section>
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/parallax.js"></script>
+<script src="js/contact.js"></script>
+<script src="js/countto.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
